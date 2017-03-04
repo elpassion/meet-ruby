b7066d5bfdca6b786db0972d9ee749a7d2c57be9
grzegorzkemski <gregkem@gmail.com>
2017-03-04 19:36:51 +0100

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'landing_pages#index'
-  post '/game_sessions' => 'game_sessions#create'
-  get '/game_sessions/:id' => 'game_sessions#show', as: :game_session
-  patch '/game_sessions/:id' => 'game_sessions#update', as: :update_game_session
+  root 'landing_pages#index'
+
+  resources :game_sessions, only: [:create, :show, :update]
 end

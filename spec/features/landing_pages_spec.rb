90f39e2704378bfb32d60bc04e0be3850f29a1f8
Karol Sarnacki <karol.sarnacki@elpassion.com>
2017-03-04 13:51:11 +0100

    Add cards to Landing Page view

--- spec/features/landing_pages_spec.rb
+++ spec/features/landing_pages_spec.rb
@@ -9,4 +9,8 @@ RSpec.feature 'Landing Pages', type: :feature do
     expect(page).to have_content 'Black Jack'
   end
 
+  it 'displays cards in 4 different suits' do
+    expect(page).to have_content 'J ♣ Q ♦ K ♥ A ♠'
+  end
+
 end
